7ab77201198c87a1abbdefffdc6f7f2fd2590b47
victorbonomi16 <bononikis@gmail.com>
2020-10-28 15:39:34 -0300

    feat: Exercicio 01 Concludo

---
+++ exercicio01.py
@@ -0,0 +1,19 @@
+numero = int(input("Informe um número: "))
+lista = [1,2,3,4]
+
+if numero < 0: 
+    lista[3] = lista[2]
+    lista[2] = lista[1]
+    lista[1] = lista[0]
+    del lista[0]
+    lista.insert(0,"4")
+    print(lista) 
+elif numero> 0: 
+    lista[0] = lista[1]
+    lista[1] = lista[2]
+    lista[2] = lista[3]
+    del lista[3]
+    lista.insert(3,"1")
+    print(lista) 
+else: 
+    print(lista)
